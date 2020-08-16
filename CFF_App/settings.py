f8090614ae0797a7dc298269450fc9008f1d684d
Tyler <tillintj@gmail.com>
2020-08-16 06:17:06 -0400

    does this work now?

--- CFF_App/settings.py
+++ CFF_App/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'aAdgf354^gfw3F#$^2das'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = "cff-web-app.herokuapp.com"
+ALLOWED_HOSTS = ['cff-web-app.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
