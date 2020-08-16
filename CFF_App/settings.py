fd0878154904ff940d77372ffbb2d34eb143edbe
Tyler <tillintj@gmail.com>
2020-08-16 08:09:18 -0400

    added secret key env var

--- CFF_App/settings.py
+++ CFF_App/settings.py
@@ -22,10 +22,15 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'aAdgf354^gfw3F#$^2das'
+
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+# SECURITY WARNING: don't run with debug turned on in production!
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
+
 
 ALLOWED_HOSTS = ['cff-web-app.herokuapp.com', '127.0.0.1']
 
