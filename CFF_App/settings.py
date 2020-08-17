68ada8a42416335d88e93f0ba69d4a5f31a3799e
Tyler <tillintj@gmail.com>
2020-08-16 20:06:33 -0400

    .

--- CFF_App/settings.py
+++ CFF_App/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = os.environ.get('CFF_WebApp_Secret Key')
+SECRET_KEY = 'df%67hnndspuo45&*(.,>rtw4670hkdpsWE@#$ed56g'
 # SECURITY WARNING: don't run with debug turned on in production!
 
 
